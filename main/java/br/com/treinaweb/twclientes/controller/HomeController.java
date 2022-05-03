39249504e1e43ced66b9bf5b30ef6560f4171503
tidinho <ti.dim@hotmail.com>
2022-05-03 16:42:11 -0300

    primeiro git

---
+++ main/java/br/com/treinaweb/twclientes/controller/HomeController.java
@@ -0,0 +1,23 @@
+package br.com.treinaweb.twclientes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/mensagem")
+    public String mensagem(ModelMap modelMap) {
+        modelMap.addAttribute("mensagem", "mensagem vinda do servidor");
+        return "mensagem";
+    }
+}
